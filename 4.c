02dbc1db39889e6f249ee3f23cce17d8050cfb78
Harini <harini.vanitha25@gmail.com>
2025-05-26 19:40:15 +0530

    4.c

--- 4.c
+++ 4.c
@@ -1,3 +1,5 @@
+/*Write a program to accept a float value as input from the user and print it rounded off to 3 decimal places. 
+Input Format: A Single float value - 'num' Output Format: Display the float value with 3 precision*/
 #include<stdio.h>
 int main()
 {
