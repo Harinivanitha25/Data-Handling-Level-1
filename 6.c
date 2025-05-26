c68a0fbf28869bf13650d86ca91b13275d8e1166
Harini <harini.vanitha25@gmail.com>
2025-05-26 19:37:54 +0530

    6.c

--- 6.c
+++ 6.c
@@ -1,3 +1,6 @@
+/*Write a program to accept a float value and an integer as input. 
+The integer represents the precision. Print the float value according to the precision.
+Input Format: Two Space Separated numbers 'num' ( Float value ) and 'precision' ( Integer ) Output Format: A Single Float value with appropriate precision.*/
 #include<stdio.h>
 int main()
 {
