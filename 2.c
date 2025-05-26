f4f3d2054d6a33f699f4586e31c8d9a77aba8062
Harini <harini.vanitha25@gmail.com>
2025-05-26 19:46:30 +0530

    2.c

--- 2.c
+++ 2.c
@@ -1,3 +1,5 @@
+/*Write a program to accept a float value as input from the user and print its closest integer value. 
+Input Format: A Single float value 'num'. Output Format: Display the closest Integer.*/
 #include<stdio.h>
 int main(){
     float a;
