7f5ed1f998c65627f5b2ebc6648bff86d2b4f8b8
Harini <harini.vanitha25@gmail.com>
2025-05-26 19:39:14 +0530

    5.c

--- 5.c
+++ 5.c
@@ -1,3 +1,5 @@
+/*Write a program to accept a number as input (Octal or Hexadecimal) from the user and print it decimal equivalent. 
+Input Format: A Single Integer 'num' - denoting either a hexadecimal or octal value Output Format: Display the Integer in decimal format*/
 #include<stdio.h>
 int main()
 {
