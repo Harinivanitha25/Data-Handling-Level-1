0f3e2de37eaec6c5fb8286c91bef55085d815bb9
Harini <harini.vanitha25@gmail.com>
2025-05-26 19:45:36 +0530

    3.c

--- 3.c
+++ 3.c
@@ -1,3 +1,6 @@
+/*Write a program to accept 2 numbers as input from the user.
+Print the 2 numbers such that the second number is 17 width spaces away from the first. 
+Input Format: Two Space Separated integers - num1, num2 Output Format: Display the two numbers with correct width space and without space in between.*/
 #include<stdio.h>
 int main()
 {
