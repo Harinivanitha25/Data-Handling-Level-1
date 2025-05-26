26fa887af5b6866a02f1b08b16137121e6ff3a13
Harini <harini.vanitha25@gmail.com>
2025-05-26 19:36:46 +0530

    7.c

--- 7.c
+++ 7.c
@@ -1,3 +1,4 @@
+//Write a program to accept a float value input. Print it with a width of 15 and rounded off to 2 decimal places. Input Format: A Single Float value Output Format: Display the Float value with 15 width and 2 precision
 #include<stdio.h>
 int main()
 {  float a;
